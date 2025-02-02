4450068a40a568f7bd18e873114be29f8604d1ba
Farrelnashwan <akhmadfarrelnashwan831@gmail.com>
2025-02-02 20:31:51 +0700

    Add files via upload

---
+++ berita.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const banner = document.querySelector(".banner");
+    banner.addEventListener("click", function() {
+        alert("Selamat datang di Ruang Pangan Indonesia!");
+    });
+});
